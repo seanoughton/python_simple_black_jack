5e985dc641f417ffeeb254746a524eee3148554b
seanoughton <seanoughton@gmail.com>
2019-02-14 15:07:44 -0800

    creating more tests

--- black_jack.py
+++ black_jack.py
@@ -1,24 +1,100 @@
-class Game:
-    def __init__(self):
-        self.deck = []
-        self.player = {}
-        self.dealer = {}
+class Card:
+    def __init__(self,suit="Hearts",value=2,face='2'):
+        self.suit = suit
+        self.value = value
+        self.face = face
+
+class Hand:
+    def __init__(self,cards=[]):
+        self.cards = cards
+
+    def busted():
+        pass
+
+class Deck:
+    def __init__(self,cards=[]):
+        self.cards = cards
+
+    def create_deck():
+        pass
+
+    def deal_cards():
+        pass
+
+class Chip:
+    def __init__(self,value):
+        self.value = value
 
 class Player:
-  def __init__(self):
-    self.chips = []
-    self.hand = {}
-    self.name = 'Player'
+    def __init__(self):
+        self.chips = []
+        self.hand = {}
+        self.name = 'Player'
+
+    def bet():
+        pass
+
+    def hit():
+        pass
+
+    def return_bank():
+        return self.chips * 25
+
+    def add_chips(num):
+        pass
+
+    def remove_chips(num):
+        pass
 
 class Dealer:
     def __init__(self):
         self.hand = {}
         self.name = 'Dealer'
 
+    def hit():
+        pass
+
+
+class Game:
+    def __init__(self):
+        self.deck = []
+        self.player = {}
+        self.dealer = {}
+
+    def won_test():
+        pass
+
+
 class Controller:
     def __init__(self):
         self.game = Game()
         self.player = Player()
         self.dealer = Dealer()
+
+    def init_game():
+        pass
+    def player_play():
+        pass
+    def dealer_play():
+        pass
+    def check_bust():
+        pass
+    def check_won():
+        pass
+
+
+
+class View:
+    def __init__(self):
+        self.name = 'View'
+
+    def display_hand():
+        pass
+
+    def display_game_won():
+        pass
+
+
+
 
 controller = Controller()
--- tests.py
+++ tests.py
@@ -1,69 +1,98 @@
 
 import unittest
 import black_jack
-from black_jack import Game,Player,Dealer,Controller
+from black_jack import Game,Player,Dealer,Controller,View,Card,Deck,Chip,Hand
 
 game = Game()
 player = Player()
 dealer = Dealer()
 controller = Controller()
+view = View()
+card = Card()
+deck = Deck()
+chip = Chip(25)
+hand = Hand()
 
 class TestGame(unittest.TestCase):
-    #CAN CREATE CONTROLLER
-    def test_create_controller(self):
-        result = controller
-        self.assertEqual(type(result),type(controller))
-    #CAN CREATE VIEW
+#CAN CREATE CARDS
+    def test_create_card(self):
+        result = black_jack.Card()
+        self.assertEqual(type(result),type(card))
 
-    #CAN CREATE A GAME
+#DECK TESTS
+    def test_create_deck(self):
+        result = black_jack.Deck()
+        self.assertEqual(type(result),type(deck))
+
+    #deck has 52 cards
+    #deck is shuffled
+    #deck can deal cards to a player
+
+
+
+
+#CHIP TESTS
+    def test_create_chip(self):
+        result = black_jack.Chip(25)
+        self.assertEqual(type(result),type(chip))
+
+#HAND TESTS
+    def test_create_hand(self):
+        result = black_jack.Hand()
+        self.assertEqual(type(result),type(hand))
+
+    #hand busted
+    #hand is black jack
+
+#GAME TESTS
     def test_create_game(self):
         result = controller.game
         self.assertEqual(type(result),type(game))
 
-    #PLAYER AND DEALER ARE CREATED
+#PLAYER TESTS
     def test_create_player(self):
         result = controller.player
         self.assertEqual(type(result),type(player))
 
+#DEALER TESTS
     def test_create_dealer(self):
         result = controller.dealer
         self.assertEqual(type(result),type(dealer))
 
-    #CREATE CARDS
-    #CREATE DECK
-    #CREATE CHIPS
-    #CREATE HAND
+#VIEW TESTS
+    def test_create_view(self):
+        result = black_jack.View()
+        self.assertEqual(type(result),type(view))
+
+    # view can display the players hand: display_hand(hand,player)
+    # view can display the dealers hand: display_hand(hand,dealer)
+    # view can display won the game information: display_game_won(game,player,dealer)
 
 
-    #GIVE PLAYER CHIPS
-    #PLAYER MAKES BET
-    #DEAL CARDS (TO PLAYER AND DEALER)
+#CONTROLLER TESTS
+    def test_create_controller(self):
+        result = black_jack.Controller()
+        self.assertEqual(type(result),type(controller))
+
+    # controller has game,player,dealer
+    # controller can initialize a game :init_game()
+    # controller initializes the player's play :player_play()
+    # controller initializes the dealers's play :dealer_play()
+    # controller initializes hand logic if hand is busted :check_bust()
+    # controller initializes game logic if game is won: check_won()
+
+    #controller GIVES PLAYER CHIPS
+    #controller initailizes the PLAYER MAKES BET
+    #controller DEAL CARDS (TO PLAYER AND DEALER)
+    #controller SHOW CARDS ( DIFFERENT FOR DEALER VS PLAYER)
+    #controller PLAYER HIT UNTIL STAY
+    #controller CHECK FOR BUST OR WIN
+    #controller DEALER HIT UNTIL 21 OR BUST
+    #controller SHOW CARDS AND FINAL SCORE
+    #controller ADJUST PLAYERS BANK (ADD OR REMOVE CHIPS)
+    #controller ASK IF THE PLAYER WANTS TO PLAY AGAIN
 
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class TestCap(unittest.TestCase):
-#
-#     def test_one_word(self):
-#         text = 'python'
-#         result = black_jack.cap_text(text)
-#         self.assertEqual(result, 'Python')
-#
-#     def test_multiple_words(self):
-#         text = 'monty python'
-#         result = black_jack.cap_text(text)
-#         self.assertEqual(result, 'Monty Python')
