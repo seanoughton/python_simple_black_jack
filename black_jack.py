7c7a5d99d170f0b7bee5a08f1bd62902435fe83e
seanoughton <seanoughton@gmail.com>
2019-02-18 16:00:19 -0800

    fixing bugs and cleaning up code

--- black_jack.py
+++ black_jack.py
@@ -135,20 +135,22 @@ class Player:
     def create_bank(self,num,value):
         self.bank.add_chips(num,value)
 
-    def bet(self,value):
-        #check to make sure that the player has enough in the bank to make the bet
-        #if so, return the value
-        #else, return an error message
-        if value <= self.bank.show_bank():
-            return value
-        else:
+    def enough_for_bet(self,value):
+        if self.bank.show_bank() < value or value < 1:
             return False
+        else:
+            return True
 
+    def bet(self,view):
+        value = 0
+        while self.enough_for_bet(value) == False:
+            value = view.place_bet()
+        return value
 
 
     def __del__(self):
-        self.hand = {}
-        self.bank = {}
+        self.hand = Hand()
+        self.bank = Bank()
 
 class Dealer:
     def __init__(self):
@@ -165,13 +167,76 @@ class Dealer:
 class Game:
     def __init__(self):
         self.deck = Deck()
-        self.player = Player()
-        self.dealer = Dealer()
         self.bet = 0
         self.deck.add_cards()
+        self.turn = 'Player'
 
-    def won_test():
+
+    def has_black_jack(self):
         pass
+
+    def player_play(self,view,player,dealer):
+        response = view.hit()
+        while response == True and self.game_over(view,player,dealer) == False:
+            player_cards = dealer.deal_cards(1,self.deck)
+            player.hand.add_cards(player_cards)
+            view.display_hand(player)
+
+            if self.game_over(view,player,dealer) == True:
+                break
+
+            response = view.hit()
+        self.turn = "Dealer"
+
+    def dealer_play(self,view,player,dealer):
+        while dealer.hand.total() < 21 and self.game_over(view,player,dealer) == False:
+            dealer_card = dealer.deal_cards(1,self.deck)
+            dealer.hand.add_cards(dealer_card)
+
+            if self.game_over(view,player,dealer) == True:
+                break
+
+
+    def game_over(self,view,player,dealer,winner={}):
+        num = int(self.bet/25)
+        response = False
+        if self.turn == 'Player':
+            if player.hand.busted() == True:
+                winner = dealer
+                view.display_busted(player)
+                response = True
+            if player.hand.black_jack() == True:
+                winner = player
+                response =  True
+
+        if self.turn == "Dealer":
+
+            if dealer.hand.busted() == True:
+                winner = player
+                view.display_busted(dealer)
+                response = True
+
+            if dealer.hand.black_jack() == True:
+                winner = dealer
+                response = True
+
+            if player.hand.total() < dealer.hand.total():
+                winner = dealer
+                response =  True
+
+
+
+
+        if response == True:
+            if winner == "Player":
+                player.bank.add_chips(num,25)
+            else:
+                player.bank.subtract_chips(num,25)
+            view.display_game_over(winner,player,dealer)
+
+        return response
+
+
 
     def __del__(self):
         self.deck = Deck()
@@ -184,19 +249,11 @@ class Controller:
         self.view = View()
 
 
-
     def init_game(self):
-        #add money to the players bank
+
+        #PLAYER SETUP
         self.player.create_bank(100,25)
-
-
-
-        # ASK THE PLAYER TO BET AND VALIDATE THE BET AMOUNT
-        value = self.view.place_bet()
-        while self.player.bet(value) == False:
-            value = self.view.place_bet()
-        else:
-            self.game.bet = value
+        self.game.bet = self.player.bet(view=self.view)
 
         #DEAL CARDS
         player_cards = self.dealer.deal_cards(2,self.game.deck)
@@ -205,51 +262,14 @@ class Controller:
         dealer_cards = self.dealer.deal_cards(2,self.game.deck)
         self.dealer.hand.add_cards(dealer_cards)
 
-        #show cards (player all, dealer one)
-        print(self.view.display_bank(self.player))
-        print(self.view.display_hand(self.player))
-        print(self.view.display_hand(self.dealer))
+        #SHOW CARDS (player all, dealer one)
+        self.view.display_bank(self.player)
+        self.view.display_hand(self.player)
+        self.view.display_hand(self.dealer)
 
-        self.player_play()
-        self.dealer_play()
-        self.check_won()
+        self.game.player_play(view=self.view,player=self.player,dealer=self.dealer)
+        # self.game.dealer_play(view=self.view,player=self.player,dealer=self.dealer)
 
-
-
-    def player_play(self):
-        response = True
-        while response == True:
-            response = self.view.hit()
-            if response == True:
-                player_cards = self.dealer.deal_cards(1,self.game.deck)
-                self.player.hand.add_cards(player_cards)
-            if self.player.hand.busted() == True:
-                self.view.display_busted()
-                break
-            if self.player.hand.black_jack() == True:
-                self.view.display_game_over(self.player,self.dealer)
-                break
-
-    def dealer_play(self):
-        while self.dealer.hand.total() < 21:
-            dealer_card = self.dealer.deal_cards(1,self.game.deck)
-            self.dealer.hand.add_cards(dealer_card)
-            if self.dealer.hand.busted() == True:
-                self.view.display_busted(self.dealer)
-                break
-            if self.dealer.hand.black_jack() == True:
-                self.view.display_game_over(self.player,self.dealer)
-                break
-
-    def check_won(self):
-        winner = {}
-        if self.player.hand.total() > self.dealer.hand.total():
-            winner = self.player
-            num = self.bet/25
-            self.player.bank.add_chips(num,25)
-        else:
-            winner = self.dealer
-        self.view.display_game_over(winner,self.player,self.dealer)
 
 
 
@@ -265,7 +285,7 @@ class View:
         self.name = 'View'
 
     def display_bank(self,player):
-        return f'Your Bank is: {player.bank.show_bank()}'
+        print (f'Your Bank is: {player.bank.show_bank()}')
 
     def place_bet(self):
         #ADD ERROR CHECKING FOR IF THEY ENTER SOMETHING OTHER THAN A NUMBER
@@ -281,13 +301,13 @@ class View:
             for card in player.hand.cards:
                 response += f" {card.face} of {card.suit}\n"
 
-        return response
+        print(response)
 
     def display_busted(self,game_player):
         if game_player.name == "Player":
-            return 'You Busted!'
+            print('You Busted!')
         else:
-            return 'Dealer Busted'
+            print ('Dealer Busted')
 
     def hit(self):
         response = ''
@@ -299,7 +319,10 @@ class View:
           return False
 
     def display_game_over(self,winner,player,dealer):
-        return f'{winner.name} is the Winner!\n Your bank is now {self.display_bank(player)}'
+        print(f'{winner.name} is the Winner!\n')
+        print(f'The Dealers Hand is: {self.display_hand(dealer)}')
+        print(f'Your Hand is: {self.display_hand(player)}')
+        print(f'Your bank is now {self.display_bank(player)}')
 
 
 
@@ -310,3 +333,4 @@ class View:
 
 
 controller = Controller()
+controller.init_game()
